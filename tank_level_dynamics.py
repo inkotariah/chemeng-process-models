c6b867fac93af2a29bf273d5b336c02aaa298905
inkotariah <inkotari@ualberta.ca>
2025-12-24 16:30:59 -0700

    dynamic tank level model and numerical simulation

---
+++ tank_level_dynamics.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+#Parameters
+A = 1.0
+Qin = 0.5
+c = 0.3
+
+#Tank level calculation
+def tank_level(t,h):
+    Qout = c*np.sqrt(h)
+    dh_dt = (Qin - Qout)/A
+    return dh_dt
+
+Time_span = (0,60)
+num_points = 300
+h0 = [0.2] #Initial height
+
+soln = solve_ivp(tank_level, Time_span, h0, dense_output=True) #Numerical computation
+
+#Processing
+t = np.linspace(Time_span[0], Time_span[1], num_points)
+h = soln.sol(t)[0]
+
+#Plotting
+plt.figure()
+plt.plot(t,h)
+plt.ylabel("Tank Level (m)")
+plt.xlabel("Time (s)")
+plt.title("Dynamic Tank Level Response")
+plt.grid()
+plt.show()
